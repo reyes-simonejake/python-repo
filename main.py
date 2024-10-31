398f9acd9baa4f499ad6feca4b89bfaa3e528ff7
reyes-simonejake <reyesjake262@gmail.com>
2024-10-31 13:46:40 +0800

    Upload this main python file

---
+++ main.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the seconf number: "))
+choice = []
+print(f"Enter your operator : {choice}+ , - , * , /")
+
+
+def calcu():
+    if choice ==  '+':
+        print('You have choose addition')
+        print (num1 + num2)
+    
